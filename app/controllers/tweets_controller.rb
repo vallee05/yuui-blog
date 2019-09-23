23c4ce693ae3542c0cecaec458a1db382447e47a
vallee05 <voluptuous.666@gmail.com>
2019-09-23 17:08:39 +0900

    新規登録作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
+
   def index
+    @tweets = Tweet.order("created_id DESC").page(params[:page]).per(5)
   end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
